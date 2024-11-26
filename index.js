23fec891373e2788be04f9a6b159e6d7a66131e4
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-26 20:31:41 +0400

    Changed code order

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-require('dotenv').config();
-
+const cors = require('cors');
 const Note = require('./models/note');
-
-let notes = [];
-
-app.use(express.static('dist'));
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
@@ -16,16 +12,10 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-const cors = require('cors');
-
 app.use(cors());
-
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(requestLogger);
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
@@ -50,7 +40,7 @@ app.post('/api/notes', (request, response) => {
   });
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
@@ -59,10 +49,7 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: 'malformatted id' });
-    });
+    .catch((error) => next(error));
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -72,7 +59,23 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end();
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
