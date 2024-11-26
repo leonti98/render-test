8dfbed0bbb6a986486d194f84cda36b0e9fbbedb
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-26 21:20:37 +0400

    Add PUT endpoint and refactor DELETE endpoint
    - Implemented a new PUT endpoint to update notes in the database.
    - Refactored the DELETE endpoint to remove notes from the database using `findByIdAndRemove`.

--- index.js
+++ index.js
@@ -52,11 +52,27 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-  response.status(204).end();
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
