2524cc79235dd65e3d367d4d2f12188eb7b8d97c
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-28 04:40:06 +0400

    Extend eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
 import globals from 'globals';
+import stylisticJs from '@stylistic/eslint-plugin-js';
+import js from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  js.configs.recommended,
   {
     files: ['**/*.js'],
     languageOptions: {
@@ -10,6 +13,15 @@ export default [
         ...globals.node,
       },
       ecmaVersion: 'latest',
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
     },
   },
 ];
