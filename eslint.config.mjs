e03829a9cc63a65a0bcea6ecc52867cb32afcc9a
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-28 04:29:45 +0400

    ESlint added

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+    },
+  },
+];
