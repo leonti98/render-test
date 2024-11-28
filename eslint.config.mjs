3eabc3bf5c9faa9de5335ae77eb1dcf9f7f5a1c1
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-28 04:50:05 +0400

    Changed linter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,9 @@ import globals from 'globals';
 import stylisticJs from '@stylistic/eslint-plugin-js';
 import js from '@eslint/js';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
-
   {
-    ignores: ['dist/**'],
     files: ['**/*.js'],
     languageOptions: {
       sourceType: 'commonjs',
@@ -24,6 +21,14 @@ export default [
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     },
+  },
+  {
+    ignores: ['dist/**', 'build/**'],
   },
 ];
